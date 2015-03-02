c48771b55d63a8dcc2b97283d241e663db0272cf
Derek Caprani <dcaprani@eircom.net>
2015-03-02 20:48:38 +0000

    Puppet manifest file

---
+++ manifests/default.pp
@@ -0,0 +1,17 @@
+exec{"apt-get update":
+	path => "/usr/bin",
+}
+package{"apache2":
+	ensure => present
+	require => Exec["apt-get update"],
+}
+service{"apache2":
+	ensure => "running",
+	require => Package["apache2"],
+}
+file{"/var/www/project1";
+	ensure => "link",
+	target => "/vagrant/project1",
+	require => Package["apache2"],
+	notify => Service["apache2"],
+}
